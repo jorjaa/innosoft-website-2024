e62896b88ed5f2aa85628b1d778e56da12ad1f25
Phuminan4238 <phuminan@live.com>
2024-04-20 18:32:08 +0700

    Create Underconstruction Pages

---
+++ components/navbar.js
@@ -0,0 +1,146 @@
+import Link from "next/link";
+import ThemeChanger from "./DarkSwitch";
+import Image from "next/image";
+import { Disclosure } from "@headlessui/react";
+
+const Navbar = () => {
+  const navigation = ["Product", "Features", "Pricing", "Company", "Blog"];
+
+  return (
+    <div className="w-full border-b">
+      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+        {/* Logo  */}
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
+                <Link href="/">
+                  <span className="flex items-center space-x-2 text-3xl font-medium dark:text-gray-100">
+                    <span>
+                      <Image
+                        src="/img/innosoft-logo.png"
+                        alt="N"
+                        width="32"
+                        height="32"
+                        className="w-8"
+                      />
+                    </span>
+                    <span className="ps-2">
+                      <span className="text-gray-500">Inno</span>
+                      <span className="text-orange-500">soft</span>
+                    </span>
+                  </span>
+                </Link>
+
+                <Disclosure.Button
+                  aria-label="Toggle Menu"
+                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-orange-500 focus:text-orange-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
+                >
+                  <svg
+                    className="w-6 h-6 fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    {open && (
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                      />
+                    )}
+                    {!open && (
+                      <path
+                        fillRule="evenodd"
+                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                      />
+                    )}
+                  </svg>
+                </Disclosure.Button>
+
+                <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
+                  <>
+                    {navigation.map((item, index) => (
+                      <Link
+                        key={index}
+                        href="/"
+                        className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-orange-500 focus:text-orange-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none"
+                      >
+                        {item}
+                      </Link>
+                    ))}
+                    <Link
+                      href="/"
+                      className="w-full px-6 py-2 mt-3 text-center text-white bg-orange-500 rounded-md lg:ml-5"
+                    >
+                      Get Started
+                    </Link>
+                  </>
+                </Disclosure.Panel>
+              </div>
+            </>
+          )}
+        </Disclosure>
+
+        {/* menu  */}
+        <div className="hidden text-center lg:flex lg:items-center">
+          <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
+            {navigation.map((menu, index) => (
+              <li className="mr-3 nav__item" key={index}>
+                <Link
+                  href="/"
+                  className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
+                >
+                  {/* {menu} */}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="hidden mr-3 space-x-4 lg:flex nav__item">
+          <Link
+            href="/"
+            className="flex items-center px-4 py-1 text-white font-light text-md text-blue-500 rounded-2xl md:ml-5"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 mr-2"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15 2H5a3 3 0 00-3 3v8a3 3 0 003 3h3l3 3v-3h4a3 3 0 003-3V5a3 3 0 00-3-3zm-1 10H6a1 1 0 00-1 1v1l-2-2 2-2v1h7a1 1 0 001-1v-2zm1-6H5a1 1 0 00-1 1v6a1 1 0 001 1h10a1 1 0 001-1V7a1 1 0 00-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span>Let's talk</span>
+          </Link>
+
+          <Link
+            href="/"
+            className="flex items-center px-5 py-1 text-white font-light text-md bg-orange-600 rounded-2xl md:ml-5"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 mr-2"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.707 3.293a1 1 0 011.414 0l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414-1.414L14.586 11H4a1 1 0 010-2h10.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span>go’s version</span>
+          </Link>
+
+          <ThemeChanger />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
